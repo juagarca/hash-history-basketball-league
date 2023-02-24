d87d475f369cb17cc5bea996fc6ecaa9eaadf863
Juan Garcia <juagarca@gmail.com>
2023-02-24 17:02:34 +0000

    Add routing and initial components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
-export default function App () {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Players from "./Players";
+import Teams from "./Teams";
+
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
-  )
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/players" element={<Players />} />
+        <Route path="/teams" element={<Teams />} />
+      </Routes>
+    </Router>
+  );
 }
